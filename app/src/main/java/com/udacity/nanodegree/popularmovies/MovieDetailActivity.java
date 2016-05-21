0c03663a5ff57c8cc7840c0449ca5376e2c7e845
sapangarg <sapangarg007@gmail.com>
2016-05-21 14:09:41 +0530

    Trailers to play on youtube after clicking on Watch Trailer link

--- app/src/main/java/com/udacity/nanodegree/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/udacity/nanodegree/popularmovies/MovieDetailActivity.java
@@ -6,6 +6,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -32,16 +34,16 @@ public class MovieDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
-        init(intent);
         Long movieId = intent.getLongExtra(MainActivity.EXTRA_MSG6, 1);
         MOVIE_TRAILERS_URL_EXPANDED = MOVIE_TRAILERS_URL.replace("{id}", movieId.toString());
         new getMovieTrailers().execute();
     }
 
-    private void init(Intent intent)
+    private void init(Intent intent, String youtube_link)
     {
         Context context = getApplicationContext();
         TextView tv_movie_title = (TextView)findViewById(R.id.movie_heading);
@@ -52,6 +54,9 @@ public class MovieDetailActivity extends AppCompatActivity {
         tv_movie_release_date.setText(intent.getStringExtra(MainActivity.EXTRA_MSG5));
         TextView tv_movie_rating = (TextView)findViewById(R.id.movie_rating);
         tv_movie_rating.setText(intent.getStringExtra(MainActivity.EXTRA_MSG4) + " / 10");
+        TextView tv_movie_trailer = (TextView)findViewById(R.id.movie_trailer);
+        tv_movie_trailer.setText(Html.fromHtml("<a href=\"" + youtube_link + "\">" + "Watch Trailer" + "</a>"));
+        tv_movie_trailer.setMovementMethod(LinkMovementMethod.getInstance());
         TextView tv_movie_plot = (TextView)findViewById(R.id.movie_plot);
         tv_movie_plot.setText(intent.getStringExtra(MainActivity.EXTRA_MSG3));
 
@@ -85,6 +90,8 @@ public class MovieDetailActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             Log.i(TAG, result);
+            Intent intent = getIntent();
+            init(intent, result);
         }
 
     }
